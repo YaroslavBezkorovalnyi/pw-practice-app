9ace63804ba460e9727deb8f307f86a689027c49
YaroslavBezkorovalnyi <my1qatesting@gmail.com>
2025-03-07 13:15:49 +0200

    updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,8 @@ require('dotenv').config();
 export default defineConfig<TestOptions>({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
+  forbidOnly: false,
+  // forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
